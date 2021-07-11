307051191a96152fd2b56828d9ba91410d9341eb
tennashi <tennashio@gmail.com>
2021-07-12 00:54:56 +0900

    Add: add the route() function

--- router.ts
+++ router.ts
@@ -1,8 +1,13 @@
+import { Denops } from "./deps.ts";
 import { Buffer } from "./buffer.ts";
 
 export type PathParams = { [key: string]: string };
 
 export type Component = (params: PathParams) => Promise<Buffer>;
+
+export async function route(denops: Denops, path: string) {
+  await denops.cmd(`bd! | new denopsapp://${denops.name}${path}`);
+}
 
 interface Route {
   path: string;
